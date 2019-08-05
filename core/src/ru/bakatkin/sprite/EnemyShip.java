4f2b539147f97ed744bc84181e13044c59ef1998
Bakatkin <bakatkin@gmail.com>
2019-08-05 21:59:07 +0300

    1. Готовый проект

--- core/src/ru/bakatkin/sprite/EnemyShip.java
+++ core/src/ru/bakatkin/sprite/EnemyShip.java
@@ -1,34 +1,37 @@
 package ru.bakatkin.sprite;
 
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-
 import ru.bakatkin.base.Ship;
 import ru.bakatkin.math.Rect;
 import ru.bakatkin.pool.BulletPool;
+import ru.bakatkin.pool.ExplosionPool;
 
 public class EnemyShip extends Ship {
 
     private enum State {DESCENT, FIGHT}
     private State state;
+    private SpaceShip spaceShip;
 
-    private final float SPEED_FIGHT = 0.005f;
-    private final float SPEED_DESCENT = 0.05f;
+    private final float SPEED_FIGHT = 0.001f;
+    private final float SPEED_DESCENT = 0.01f;
 
-    public EnemyShip(BulletPool bulletPool, Rect worldBounds) {
+    public EnemyShip(BulletPool bulletPool, ExplosionPool explosionPool, Rect worldBounds, SpaceShip spaceShip) {
         this.bulletPool = bulletPool;
+        this.worldBounds = worldBounds;
+        this.explosionPool = explosionPool;
+        this.spaceShip = spaceShip;
         bulletSound = Gdx.audio.newSound(Gdx.files.internal("sound/laser.wav"));
         move = new Vector2();
         bulletVelocity = new Vector2();
-        this.worldBounds = worldBounds;
-        reloadInterval = 0.01f;
+        reloadInterval = 0f;
         state = State.DESCENT;
     }
 
     @Override
     public void update(float delta) {
+        super.update(delta);
         switch (state) {
             case DESCENT:
                 if (getTop() <= worldBounds.getTop()) {
@@ -45,6 +48,8 @@ public class EnemyShip extends Ship {
                 }
                 move(SPEED_FIGHT);
                 if (getBottom() < worldBounds.getBottom()) {
+                    spaceShip.damage(this.damage);
+                    boom();
                     destroy();
                 }
                 break;
@@ -74,5 +79,14 @@ public class EnemyShip extends Ship {
         setHeightProportion(height);
         this.hp = hp;
         state = State.DESCENT;
+    }
+
+    public boolean isBulletCollision(Rect bullet){
+        return !(
+                bullet.getRight() < getLeft()
+                || bullet.getLeft() > getRight()
+                ||bullet.getBottom() > getTop()
+                || bullet.getTop() < pos.y
+                );
     }
 }
